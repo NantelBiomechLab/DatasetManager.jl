1cc33ae824697a3455fcf04ef8a142970965d9fb
Allen Hill <allenofthehills@gmail.com>
2020-12-27 21:36:51 -0500

    Rename path to sourcepath

--- src/DatasetManager.jl
+++ src/DatasetManager.jl
@@ -5,7 +5,7 @@ using Glob
 export DataSubset, TrialConditions, Trial, DuplicateSourceError, Segment, SegmentResult,
     AbstractSource
 
-export findtrials, readsource, readsegment, path
+export findtrials, readsource, readsegment, sourcepath
 
 include("source.jl")
 include("trial.jl")
