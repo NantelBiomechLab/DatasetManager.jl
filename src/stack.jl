bf868be1a287b5e4460a30100dbf3a18881a5d12
Allen Hill <allenofthehills@gmail.com>
2021-08-09 11:30:12 -0400

    Add method for TrialConditions

--- src/stack.jl
+++ src/stack.jl
@@ -3,20 +3,22 @@
 
 Compile the results into a stacked, long form DataFrame
 """
-function stack(rs::Vector{SegmentResult{S,ID}}, conds;
+function stack(rs::Vector{<:SegmentResult}, conds;
     variables=sort(collect(keys(first(rs).results)))
-) where {S,ID}
+)
     df = DataFrame(subject = categorical(subject.(rs)))
 
     for cond in conds
-        setproperty!(df, cond, categorical(getindex.(conditions.(rs), cond)))
+        insertcols!(df, cond => categorical(getindex.(conditions.(rs), cond)))
     end
 
     for var in variables
-        setproperty!(df, var, getindex.(getfield.(rs, :results), var))
+        insertcols!(df, var => getindex.(getfield.(rs, :results), var))
     end
 
     return sort!(DataFrames.stack(df, Not([:subject, conds...])),
         [:variable, :subject, conds...])
 end
+
+stack(rs::Vector{<:SegmentResult}, conds::TrialConditions; kwargs...) = stack(rs, conds.condnames; kwargs...)
 
