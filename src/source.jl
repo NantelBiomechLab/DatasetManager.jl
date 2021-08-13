49bcd047fc36e1dcbda8c25a4b31025bad476bf3
Allen Hill <allenofthehills@gmail.com>
2021-08-13 17:53:09 -0400

    Massive expansion of Source related APIs
    - Add generic `Source` type parameterized by singleton types
        - Simplifies the definition of new, basic sources and enables
          convenient reuse of existing types (e.g. `C3DFile`)
    - Define empty constructors for all `S<:AbstractSource` (file in /tmp)
        - Add optional function to define the standard file extension for a
          source in `srcext`; default is an empty string
    - Add `requiresource!` function and related interfaces
        - Enables explicit dependencies on particular sources/source types,
          of value in in, for example, analysis functions
        - Optional auto-generation of capable sources
        - Complete list of new Source related interfaces (all prompted by
          `requiresource!` development):
            - `requiresource!`: Described above
            - `dependencies`: Define dependencies for a source, defaults to
              none (ie cannot be generated)
            - `generatesource`: Function stub; must be defined for
              auto-generation of any given `S<:AbstractSource`
            - `srcname_default`: Optional definition of typical and more
              user friendly default name for a source (used as the source
              key in the `Trial.sources`
            - `srcext`: Optional definition of standard file extension for a
              source; default is empty string

--- src/source.jl
+++ src/source.jl
@@ -24,6 +24,14 @@ DatasetManager.readsegment(seg::Segment{MySource}) = readsource(seg.source)[star
 """
 abstract type AbstractSource end
 
+(::Type{S})() where S <: AbstractSource = S(tempname()*srcext(S))
+
+struct Source{S} <: AbstractSource
+    path::String
+end
+
+Source{S}() where S = Source{S}(tempname()*srcext(Source{S}))
+
 """
     sourcepath(src) -> String
 
@@ -31,9 +39,72 @@ Return the absolute path to the `src`.
 """
 sourcepath(src::S) where S <: AbstractSource = src.path
 
+srcext(src::S) where S <: AbstractSource = splitext(sourcepath(src))[2]
+srcext(::Type{<:AbstractSource}) = ""
+
 """
     readsource(src::S; kwargs...) where {S <: AbstractSource}
 
 Read the source data from storage.
 """
 function readsource end
+
+struct UnknownDeps end
+dependencies(::S) where S <: AbstractSource = dependencies(S)
+dependencies(::Type{<:AbstractSource}) = UnknownDeps()
+
+srcname_default(::S) where S <: AbstractSource = srcname_default(S)
+srcname_default(s) = string(s)
+
+struct MissingSourceError <: Exception
+    msg::String
+end
+
+function Base.showerror(io::IO, e::MissingSourceError)
+    print(io, "MissingSourceError: ")
+    print(io, e.msg)
+end
+
+function requiresource!(trial, src::S, parent=nothing; kwargs...) where S <: AbstractSource
+    requiresource!(trial, srcname_default(src) => src, parent; kwargs...)
+end
+
+function requiresource!(trial, src::Type{<:AbstractSource}, parent=nothing; kwargs...)
+    requiresource!(trial, srcname_default(src) => src(), parent; kwargs...)
+end
+
+function requiresource!(trial, namesrc::Pair, parent=nothing;
+    force=false, deps=dependencies(namesrc.second), kwargs...
+)
+    name, src = namesrc
+    if !force
+        if hassource(trial, name) || hassource(trial, src) ||
+            (parent !== nothing && hassource(trial, typeof(src)))
+            return nothing
+        elseif isfile(sourcepath(src))
+            sources(trial)[name] = src
+            return nothing
+        end
+    end
+
+    if deps isa UnknownDeps
+        throw(MissingSourceError("unable to generate missing source $src for $trial"))
+    else
+        foreach(deps) do reqsrc
+            requiresource!(trial, reqsrc, src; force=false)
+        end
+    end
+    _src = generatesource(trial, src, deps; kwargs...)
+    isfile(sourcepath(_src)) ||
+        throw(MissingSourceError("failed to generate source $name => $_src for $trial"))
+
+    sources(trial)[name] = _src
+
+    return nothing
+end
+
+function generatesource end
+
+generatesource(trial, ::Type{S}, deps; kwargs...) where S <: AbstractSource =
+    generatesource(trial, S(), deps; kwargs...)
+
