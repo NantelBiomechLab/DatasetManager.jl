304202ad0329db29b15b80cc339198e9fe9debca
Allen Hill <allenofthehills@gmail.com>
2022-06-20 16:50:11 -0400

    [matlab] Add docstrings to most matlab functions and tweak docstring extraction

--- docs/extract-matlab-docstrings.jl
+++ docs/extract-matlab-docstrings.jl
@@ -1,10 +1,6 @@
 using DatasetManager
-symbols = []
 
 function matlab_docstr(name, type, docstring, file, lines)
-    if type != "Class"
-        name = string(basename(splitext(file)[1]), '.', name)
-    end
     article = """
         ```@raw html
         <article class="docstring">
@@ -29,47 +25,88 @@ function matlab_docstr(name, type, docstring, file, lines)
     return article
 end
 
-for file in readdir(joinpath(pkgdir(DatasetManager), "matlab"))
-    str = read(joinpath(pkgdir(DatasetManager), "matlab", file), String)
+function extract_matlab_docstrings(files=readdir(joinpath(pkgdir(DatasetManager), "matlab")))
+    symbols = Dict{Symbol,String}()
 
-    for m in eachmatch(r"(?<name>(?<type>classdef|function) .*)\n(?<docstring>(([\t ]+%[\t ]*.*\n)+))", str)
-        isnothing(m) && continue
-        type = m[:type] == "classdef" ? "Class" : "Function"
-        name_rm = match(r"^(function |classdef )?(\[?(([[:alpha:]]+\w*[[:alpha:][:digit:]])(, ([[:alpha:]]+\w*[[:alpha:][:digit:]]))*)?\]? ?= ?)?(?<symbol_name>[[:alpha:]]+\w*[[:alpha:][:digit:]])", m[:name])
-        name = name_rm[:symbol_name]
+    for file in files
+        str = read(joinpath(pkgdir(DatasetManager), "matlab", file), String)
 
-        beginline = countlines(IOBuffer(SubString(str, 1, m.offset)))
-        endline = countlines(IOBuffer(m.match))
-        lines = (beginline, endline+beginline-1)
-        docstring = replace(m[:docstring], r"(\n?)( {3,}|\t+)% ?" => s"\1")
-        docstring = replace(docstring,
-            r"^\n?([[:upper:]]+\w*[[:upper:][:digit:]])  " => "")
-        docstring = replace(docstring,
-            r"<a href=\"matlab:web\('(?<url>[^']*)'\)\">(?<text>.*)<\/a>" => s"[\g<text>](\g<url>)",
-            r"# (.*)" => s"**\1**")
+        for m in eachmatch(r"(?<name>(?<type>classdef|function) .*)\n(?<docstring>(([\t ]+%[\t ]*.*\n)+))", str)
+            isnothing(m) && continue
+            type = m[:type] == "classdef" ? "Class" : "Function"
+            name_rm = match(r"^(function |classdef )?(\[?(([[:alpha:]]+\w*[[:alpha:][:digit:]])(, ([[:alpha:]]+\w*[[:alpha:][:digit:]]))*)?\]? ?= ?)?(?<symbol_name>[[:alpha:]]+\w*[[:alpha:][:digit:]])", m[:name])
+            name = name_rm[:symbol_name]
 
-        signatures = match(r"\n\n(?<sigs>(  .*\n)+)", docstring)
-        if !isnothing(signatures)
-            docstring = replace(docstring, signatures[:sigs] => "")
-            sigs = replace(signatures[:sigs], r"(\n?)  " => s"\1")
-            docstring = string("```matlab\n", sigs, "```\n", docstring)
+            beginline = countlines(IOBuffer(SubString(str, 1, m.offset)))
+            endline = countlines(IOBuffer(m.match))
+            lines = (beginline, endline+beginline-1)
+            docstring = replace(m[:docstring], r"(\n?)( {3,}|\t+)% ?" => s"\1")
+            docstring = replace(docstring,
+                r"^\n?([[:upper:]]+\w*[[:upper:][:digit:]])  " => "")
+            # @show docstring
+            docstring = replace(docstring,
+                r"<a\s+href=\"matlab:web\('(?<url>[^']*)'\)\">(?<text>[\s\S]*?)<\/a>" => s"[\g<text>](\g<url>)",
+                r"\[`([[:alpha:]]+\w*[[:alpha:][:digit:]](\.[[:alpha:]]+\w*[[:alpha:][:digit:]])?)`\]\(@ref\)" => s"[`\1`](#\1)",
+                r"#+ (.*)" => s"**\1**")
+
+            signatures = match(r"\n\n(?<sigs>(  .*\n)+)", docstring)
+            if !isnothing(signatures)
+                docstring = replace(docstring, signatures[:sigs] => "")
+                sigs = replace(signatures[:sigs], r"(\n?)  " => s"\1")
+                docstring = string("```matlab\n", sigs, "```\n", docstring)
+            end
+            seealso_rgx = r"See also (([[:alpha:]]+\w*[[:alpha:][:digit:]](\.[[:alpha:]]+\w*[[:alpha:][:digit:]])?)(, ([[:alpha:]]+\w*[[:alpha:][:digit:]](\.[[:alpha:]]+\w*[[:alpha:][:digit:]])?))*)"
+            seealso = match(seealso_rgx, docstring)
+            if !isnothing(seealso)
+                seealso_refs = join(("[$sym](#$sym)" for sym in split(seealso[1], ", ")), ", ")
+                docstring = replace(docstring, seealso[1] => seealso_refs)
+            end
+
+            line1 = readline(joinpath(pkgdir(DatasetManager), "matlab", file))
+            if occursin(r"^classdef", line1)
+                classm = match(r"^classdef (\S+)", line1)
+                namespace = string(classm[1], ".")
+            else
+                namespace = ""
+            end
+            fullsym = type == "Class" ? Symbol(name) :
+                Symbol(string(namespace, name))
+
+            symbols[fullsym] = matlab_docstr(fullsym, type, docstring, joinpath("matlab", file), lines)
         end
-        seealso_rgx = r"See also (([[:alpha:]]+\w*[[:alpha:][:digit:]](\.[[:alpha:]]+\w*[[:alpha:][:digit:]])?)(, ([[:alpha:]]+\w*[[:alpha:][:digit:]](\.[[:alpha:]]+\w*[[:alpha:][:digit:]])?))*)"
-        seealso = match(seealso_rgx, docstring)
-        if !isnothing(seealso)
-            seealso_refs = join(("[$sym](#$sym)" for sym in split(seealso[1], ", ")), ", ")
-            docstring = replace(docstring, seealso[1] => seealso_refs)
-        end
-
-
-        push!(symbols, (Symbol(name), matlab_docstr(name, type, docstring, joinpath("matlab", file), lines)))
     end
-end
 
-open(joinpath(@__DIR__, "src", "matlab-reference.md"), "w+") do io
-    println(io, "# MATLAB reference\n")
-    println.(io, last.(symbols))
-end
+    order = Symbol[
+        :TrialConditions,
+        Symbol("TrialConditions.generate"),
+        :DataSubset,
+        :Trial,
+        Symbol("Trial.findtrials"),
+        Symbol("Trial.summarize"),
+        Symbol("Trial.analyzetrials"),
+        Symbol("SegmentResult.stack"),
+        :write_results,
+        :Source,
+        Symbol("Source.readsource"),
+        Symbol("Trial.requiresource"),
+        Symbol("Source.generatesource"),
+        Symbol("Source.dependencies"),
+        Symbol("Source.srcext"),
+        Symbol("Source.srcname_default"),
+        :Segment,
+        Symbol("Segment.readsegment"),
+        :SegmentResult,
+    ]
 
-nothing
+    open(joinpath(@__DIR__, "src", "matlab-reference.md"), "w+") do io
+        println(io, "# MATLAB reference\n")
+        foreach(order) do sym
+            if haskey(symbols, sym)
+                println(io, symbols[sym])
+            end
+        end
+    end
+
+    return nothing
+end
 
--- matlab/Source.m
+++ matlab/Source.m
@@ -1,4 +1,40 @@
 classdef Source
+    % SOURCE  A type representing a source of data in a particular file format
+    %
+    %   src = Source(path)
+    %
+    % # Input arguments
+    %
+    % - `path`: The absolute path to the source file
+    %
+    % # Examples
+    %
+    % ```matlab
+    % src = Source('/path/to/source/file')
+    % ```
+    %
+    % # Creating custom sources
+    %
+    % All subtypes of `Source` **must**:
+    %
+    % - have a `path` field
+    % - have at least these two constructors:
+    %   - empty constructor
+    %   - single argument constructor accepting a char vector of an absolute path
+    %
+    % `Source` subtypes **should**:
+    %
+    % - have a [`Source.readsource`](@ref) method
+    %
+    % `Source` subtypes **may** implement these additional methods to improve user
+    % experience and/or enable additional funcitonality:
+    %
+    % - [`Source.readsegment`](@ref)
+    % - [`Source.generatesource`](@ref) (if enabling `requiresource!` generation)
+    % - [`Source.dependencies`](@ref) (if defining a `generatesource` method)
+    % - [`Source.srcext`](@ref)
+    % - [`Source.srcname_default`](@ref)
+
     properties
         path(1,:) char
     end
@@ -20,6 +56,46 @@ classdef Source
         end
 
         function ext = srcext(obj)
+            % SRCEXT  Get the file extension for a source.
+            %
+            %   ext = srcext(src)
+            %
+            % # Input arguments
+            %
+            % - `src`: Can be the class of a source subtype or an instance of a source. If
+            %   `src` is a class, the default extension for that `src < Source` class will be
+            %   returned; if `src` is an instance of a source, then the actual extension for
+            %   that `src` will be returned, regardless of whether it matches the default
+            %   extension for that class or not.
+            %
+            % # Examples
+            %
+            % ```matlab
+            % % assuming the existence of a class `GaitEvents` which subtypes `Source`
+            % src = GaitEvents('/path/to/file.tsv')
+            % ext = srcext(GaitEvents) % == '.csv'
+            % ext = srcext(src) % == '.tsv'
+            % ```
+            %
+            % # Implementation notes
+            %
+            % When defining a method for a custom class subtyping `Source`, the period
+            % should be included in the extension.
+            %
+            % ## Example implementation
+            %
+            % ```matlab
+            % function ext = srcext(obj)
+            %     %% Calling the default `srcext` method for `Source` will return the actual
+            %     %% extension for `< Source` instances
+            %     ext = srcext@Source(obj);
+            %
+            %     if isempty(ext)
+            %         ext = '.ext';
+            %     end
+            % end
+            % ```
+
             if nargin == 0
                 ext = '';
             else
@@ -28,6 +104,11 @@ classdef Source
         end
 
         function readsource(obj)
+            % READSOURCE  Read the source data from file.
+            %
+            %   data = readsource(src)
+            %
+
             error('Error: A `readsource` function has not been implemented yet for %s', ...
                 class(obj))
         end
